f8d5d3a1d81f7dd202182bd059e6a7e04bbade4a
Jose Lucas <drummond05@proton.me>
2026-04-20 01:36:27 -0300

    feat(exception): Introduce DomainInvalidException and update Email validation

---
+++ src/main/java/io/github/lucas_goncalves_tech/tech_equipment_manager/exception/DomainInvalidException.java
@@ -0,0 +1,8 @@
+package io.github.lucas_goncalves_tech.tech_equipment_manager.exception;
+
+public class DomainInvalidException extends RuntimeException {
+
+    public DomainInvalidException(String message) {
+        super(message);
+    }
+}
